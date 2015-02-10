c8c2e2633e667030a7fffa173a941876e9654b5c
dianaprzh <diany.45@gmail.com>
2015-02-10 18:33:24 -0500

    GroceryPlaner-Initial Commit

---
+++ app/src/main/java/co/mobilemakers/groceryplanner/WelcomeFragment.java
@@ -0,0 +1,23 @@
+package co.mobilemakers.groceryplanner;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class WelcomeFragment extends Fragment {
+
+    public WelcomeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_welcome, container, false);
+        return rootView;
+    }
+}
